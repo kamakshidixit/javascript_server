71323c829363cb8f3b138ce5673c2a1a09f90fd9
kamakshi7460851696 <62650874+kamakshi7460851696@users.noreply.github.com>
2020-10-23 19:28:08 +0530

    feat : Add file

---
+++ extraTs/index.ts
@@ -0,0 +1,9 @@
+//console.log("hi");
+import {diamond,equilateraltriangle} from '../extra/pattern';
+import {hasPermission,validateUsers} from '../extra/utils';
+import {permissions , users} from '../extra/constant';
+diamond(4);
+equilateraltriangle(4);
+hasPermission('getAccess','tainer','all');
+validateUsers(users);
+
