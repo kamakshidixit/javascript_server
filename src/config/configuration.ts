cca5ae799ef1cb48f45ec4742293113f8b9a81f1
kamakshidixit <72728272+kamakshidixit@users.noreply.github.com>
2020-11-19 19:20:27 +0530

    Update configuration.ts
    modify day 16 task

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,8 +1,9 @@
 import { IConfig } from './IConfig';
 // tslint:disable-next-line: no-var-requires
 const envVars = require ('dotenv').config();
-console.log('Inside config', envVars);
-const config = envVars.parsed;
+//console.log('Inside config', envVars);
+//const config = envVars.parsed;
+const config: IConfig = { port: envVars.parsed.PORT , nodeEnv: envVars.parsed.NODE_ENV, mongoUrl: envVars.parsed.MONGO_URL,  key: envVars.parsed.KEY, password: envVars.parsed.PASSWORD};
 Object.freeze(config);
 // config.PORT=7000;
 export default config;
